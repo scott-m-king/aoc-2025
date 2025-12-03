e7d1d2136be781eb2eed231f06ebfe10e8f06c4a
Scott <scott.m.king@outlook.com>
2025-12-02 22:59:02 -0800

    refactor day 2 part 2

--- src/main/kotlin/day02/Day.kt
+++ src/main/kotlin/day02/Day.kt
@@ -4,32 +4,25 @@ import kotlin.text.substring
 import kotlin.text.take
 
 class Day(val input: List<String>) {
-    private fun parseInput() = input[0].split(",")
+    private fun solve(fn: (Long, Long) -> Long) = input[0].split(",")
         .map { it.split('-') }
         .map { it[0].toLong() to it[1].toLong() }
+        .fold(0L) { acc, (l, r) -> (l..r).fold(acc) { result, num -> fn(num, result) } }
 
-    fun starOne() = parseInput()
-        .fold(0L) { acc, (l, r) ->
-            (l..r).fold(acc) { acc2, num ->
-                num.toString().run {
-                    when {
-                        length.mod(2) == 0 && take(length / 2) == substring(length / 2, length) -> acc2 + num
-                        else -> acc2
-                    }
-                }
+    fun starOne() = solve { num, acc ->
+        num.toString().run {
+            when {
+                length.mod(2) == 0 && take(length / 2) == substring(length / 2, length) -> acc + num
+                else -> acc
             }
-        }.run(::println)
+        }
+    }.run(::println)
 
-    fun starTwo() = parseInput()
-        .fold(0L) { acc, (l, r) ->
-            (l..r).fold(acc) { acc2, num ->
-                num.toString().run {
-                    for (i in (length / 2) downTo 1) {
-                        if (length.mod(i) != 0) continue
-                        if (chunked(i).toSet().size == 1) return@fold acc2 + num
-                    }
-                    acc2
-                }
-            }
-        }.run(::println)
+    fun starTwo() = solve { num, acc ->
+        num.toString().run {
+            ((length / 2) downTo 1)
+                .any { length.mod(it) == 0 && chunked(it).toSet().size == 1 }
+                .let { if (it) acc + num else acc }
+        }
+    }.run(::println)
 }
