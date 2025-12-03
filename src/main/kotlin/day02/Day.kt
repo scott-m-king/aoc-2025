6819767aaa7ec68d5d809a40824623cf89a0e854
Scott <scott.m.king@outlook.com>
2025-12-02 22:03:10 -0800

    day 2 part 1

--- src/main/kotlin/day02/Day.kt
+++ src/main/kotlin/day02/Day.kt
@@ -1,11 +1,22 @@
 package day02
 
-import java.util.Scanner
+import kotlin.text.substring
+import kotlin.text.take
 
 class Day(val input: List<String>) {
-    fun starOne() {
-        TODO()
-    }
+    fun starOne() = input[0].split(",")
+        .map { it.split('-') }
+        .map { it[0].toLong() to it[1].toLong() }
+        .fold(0L) { acc, (l, r) ->
+            (l..r).fold(acc) { acc2, num ->
+                num.toString().run {
+                    when {
+                        length % 2 == 0 && take(length / 2) == substring(length / 2, length) -> acc2 + num
+                        else -> acc2
+                    }
+                }
+            }
+        }.run(::println)
 
     fun starTwo() {
         TODO()
