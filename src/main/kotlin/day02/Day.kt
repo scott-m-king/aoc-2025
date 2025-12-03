86d8211c473e259e45fb6c66513e2511b9be1e62
Scott <scott.m.king@outlook.com>
2025-12-02 22:30:44 -0800

    day 2 part 2

--- src/main/kotlin/day02/Day.kt
+++ src/main/kotlin/day02/Day.kt
@@ -4,21 +4,32 @@ import kotlin.text.substring
 import kotlin.text.take
 
 class Day(val input: List<String>) {
-    fun starOne() = input[0].split(",")
+    private fun parseInput() = input[0].split(",")
         .map { it.split('-') }
         .map { it[0].toLong() to it[1].toLong() }
+
+    fun starOne() = parseInput()
         .fold(0L) { acc, (l, r) ->
             (l..r).fold(acc) { acc2, num ->
                 num.toString().run {
                     when {
-                        length % 2 == 0 && take(length / 2) == substring(length / 2, length) -> acc2 + num
+                        length.mod(2) == 0 && take(length / 2) == substring(length / 2, length) -> acc2 + num
                         else -> acc2
                     }
                 }
             }
         }.run(::println)
 
-    fun starTwo() {
-        TODO()
-    }
+    fun starTwo() = parseInput()
+        .fold(0L) { acc, (l, r) ->
+            (l..r).fold(acc) { acc2, num ->
+                num.toString().run {
+                    for (i in (length / 2) downTo 1) {
+                        if (length.mod(i) != 0) continue
+                        if (chunked(i).toSet().size == 1) return@fold acc2 + num
+                    }
+                    acc2
+                }
+            }
+        }.run(::println)
 }
