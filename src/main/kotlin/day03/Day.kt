f0cb0c82962f548991c1a43643a4ea545b39ebd6
Scott <scott.m.king@outlook.com>
2025-12-04 23:51:59 -0800

    day 3 refactor

--- src/main/kotlin/day03/Day.kt
+++ src/main/kotlin/day03/Day.kt
@@ -1,20 +1,17 @@
 package day03
 
 class Day(val input: List<String>) {
-    fun starOne(): Int = input.map { it.map { c -> "$c".toInt() } }.sumOf { line ->
-        line.take(line.size - 1).foldIndexed(0 to line[0]) { idx, acc, curr ->
-            if (curr > acc.second) (idx to curr)
-            else acc
-        }.let { (index, f) -> "$f${line.subList(index + 1, line.size).max()}".toInt() }
+    fun starOne(): Long = solve(2)
+
+    fun starTwo(): Long = solve(12)
+
+    fun solve(size: Int): Long = input.map { it.map { c -> "$c".toInt() } }.sumOf {
+        helper(it, "", 0, 0, size)
     }
 
-    fun starTwo(): Long = input.map { it.map { c -> "$c".toInt() } }.sumOf {
-        helper(it, "", 0, 0)
-    }
-
-    tailrec fun helper(line: List<Int>, curr: String, idx: Int, offset: Int): Long {
-        if (idx == 12) return curr.toLong()
-        val max = line.slice(offset..(line.size - 12 + idx)).withIndex().maxBy { it.value }
-        return helper(line, "$curr${max.value}", idx + 1, offset + max.index + 1)
+    tailrec fun helper(line: List<Int>, curr: String, idx: Int, offset: Int, size: Int): Long {
+        if (idx == size) return curr.toLong()
+        val max = line.slice(offset..(line.size - size + idx)).withIndex().maxBy { it.value }
+        return helper(line, "$curr${max.value}", idx + 1, offset + max.index + 1, size)
     }
 }
