bf48cc7ff854686655f2309307606cae738313a1
Scott <scott.m.king@outlook.com>
2025-12-06 11:25:42 -0800

    day 5 refactor

--- src/main/kotlin/day05/Day.kt
+++ src/main/kotlin/day05/Day.kt
@@ -2,27 +2,24 @@ package day05
 
 class Day(val input: String) {
     fun starOne(): Int = input.split("\n\n").let { part ->
-        val ranges = part[0].lines().map { it.split('-').map { num -> num.toLong() }.let { range -> range[0] to range[1] } }
+        val ranges =
+            part[0].lines().map { it.split('-').map { num -> num.toLong() }.let { range -> range[0] to range[1] } }
         val ingredients = part[1].lines().map { it.toLong() }
         ingredients.count { ranges.any { (start, end) -> it in start..end } }
     }
 
-    data class Interval(val end: Long, val result: Long)
-
-    fun starTwo(): Long {
-        val ranges = input.split("\n\n")[0].lines()
-            .map { it.split('-').map { num -> num.toLong() }.let { range -> range[0] to range[1] } }
-            .sortedBy { it.first }
-
-        val (firstStart, firstEnd) = ranges[0]
-
-        return ranges.drop(1).fold(Interval(firstEnd, firstEnd - firstStart + 1)) { acc, (currStart, currEnd) ->
-            val (end, result) = acc
-            when {
-                currEnd <= end -> acc
-                currStart > end -> Interval(end = currEnd, result = result + (currEnd - currStart + 1))
-                else -> Interval(end = currEnd, result = result + (currEnd - end))
-            }
-        }.result
-    }
+    fun starTwo(): Long = input.split("\n\n")[0].lines()
+        .map { it.split('-').map { num -> num.toLong() }.let { range -> range[0] to range[1] } }
+        .sortedBy { it.first }
+        .let {
+            val (firstStart, firstEnd) = it[0]
+            it.drop(1).fold(firstEnd to firstEnd - firstStart + 1) { acc, (currStart, currEnd) ->
+                val (end, result) = acc
+                when {
+                    currEnd <= end -> acc
+                    currStart > end -> currEnd to (result + (currEnd - currStart + 1))
+                    else -> currEnd to (result + (currEnd - end))
+                }
+            }.second
+        }
 }
